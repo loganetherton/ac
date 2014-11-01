606f55b0031d7eb95ac66f45e712dbd7e3eb72b4
logan <loganetherton@gmail.com>
2014-11-01 18:04:05 -0400

    Separating controllers

---
+++ packages/custom/tasklist/public/controllers/tasklistInsert.js
@@ -0,0 +1,51 @@
+'use strict';
+
+var app = angular.module('mean.tasklist');
+
+app.controller('TasklistInsertController',
+// Tasklist here is referring to the Mongo model
+['$scope', '$stateParams', '$location', 'Global', 'Tasklist', 'MeanSocket',
+ function ($scope, $stateParams, $location, Global, Tasklist, MeanSocket) {
+     $scope.global = Global;
+     $scope.strings = {
+         name: 'Task list', project: 'Setting up'
+     };
+     /**
+      * Check if the user has authorization
+      *
+      * @param task
+      * @returns {*}
+      */
+     $scope.hasAuthorization = function (task) {
+         if (!task || !task.user) {
+             return false;
+         }
+         return $scope.global.isAdmin || task.user._id === $scope.global.user._id;
+     };
+     /**
+      * Create a new task
+      * @param isValid
+      */
+     $scope.create = function (isValid) {
+         MeanSocket.emit('testSignal', {
+             data: 'tasklist'
+         });
+         if (isValid) {
+             var task = new Tasklist({
+                 title: this.title, content: this.content
+             });
+             task.$save(function (response) {
+                 $location.path('tasklist/' + response._id);
+             });
+
+             $scope.tasks.push({
+                 title: task.title, content: task.content
+             });
+
+             this.title = '';
+             this.content = '';
+         } else {
+             $scope.submitted = true;
+         }
+     };
+ }]);

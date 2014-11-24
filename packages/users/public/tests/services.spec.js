b6d6ee7d7cd22ef0b3a632c2491eabb8a4e53108
logan <loganetherton@gmail.com>
2014-11-24 01:44:10 -0500

    Testing RegisterService

--- packages/users/public/tests/services.spec.js
+++ packages/users/public/tests/services.spec.js
@@ -1,5 +1,10 @@
 'use strict';
 
+var user = {
+    _id: 1,
+    name: 'Baron von Bullshit'
+};
+
 describe('LoginService', function () {
     var httpBackend, deferred, loginService, scope;
     beforeEach(function () {
@@ -18,10 +23,7 @@ describe('LoginService', function () {
         beforeEach(inject(function ($q) {
             deferred = $q.defer();
             deferred.resolve({
-                user: {
-                    _id: 1,
-                    name: 'Baron von Bullshit'
-                },
+                user: user,
                 redirect: 'fake.redirect'
             });
             httpBackend.whenPOST('/login').respond(deferred.promise);
@@ -41,10 +43,7 @@ describe('LoginService', function () {
                 $$state: {
                     status: 1,
                     value: {
-                        user: {
-                            _id: 1,
-                            name: 'Baron von Bullshit'
-                        },
+                        user: user,
                         redirect: 'fake.redirect'
                     }
                 }
@@ -75,7 +74,74 @@ describe('LoginService', function () {
 });
 
 describe('RegisterService', function () {
+    var httpBackend, deferred, registerService, scope;
+    beforeEach(function () {
+        module('mean');
+        module('mean.system');
+        module('mean.users');
+    });
 
+    beforeEach(inject(function ($httpBackend, acRegisterService, $rootScope) {
+        httpBackend = $httpBackend;
+        registerService = acRegisterService;
+        scope = $rootScope.$new();
+    }));
+
+    describe('success registration', function () {
+        beforeEach(inject(function ($q) {
+            deferred = $q.defer();
+            deferred.resolve({
+                user: user,
+                redirect: 'fake.redirect'
+            });
+            httpBackend.whenPOST('/register').respond(deferred.promise);
+        }));
+        it('should emit logged in with user and redirect on success', inject(function ($rootScope) {
+            spyOn($rootScope, '$emit');
+            registerService.register({
+                email: 'fake@fake.com',
+                name: 'fakefakefake',
+                password: 'password',
+                confirmPassword: 'password'
+            });
+            httpBackend.flush();
+            scope.$digest();
+            expect($rootScope.$emit.mostRecentCall.args[0]).toEqual('loggedin');
+            var responseEmit = $rootScope.$emit.mostRecentCall.args[1];
+            expect(angular.equals(responseEmit, {
+                $$state: {
+                    status: 1,
+                    value: {
+                        user: user,
+                        redirect: 'fake.redirect'
+                    }
+                }
+            })).toBeTruthy();
+        }));
+    });
+
+    describe('unsuccessful registration', function () {
+        beforeEach(function () {
+            httpBackend.whenPOST('/register').respond(500, '');
+        });
+
+        it('should return a rejected promise on failed login', inject(function ($rootScope) {
+            spyOn($rootScope, '$emit');
+            registerService.register({
+                email: 'fake@fake.com',
+                name: 'fakefakefake',
+                password: 'password',
+                confirmPassword: 'badPassword'
+            }).then(function () {
+                expect(false).toBeTruthy();
+            }, function () {
+                expect(true).toBeTruthy();
+            });
+            httpBackend.flush();
+            scope.$digest();
+            expect($rootScope.$emit).not.toHaveBeenCalled();
+        }));
+    });
 });
 
 describe('TogglePasswordDir', function () {

992bfbf4286c82abcba8771dbddb460019287677
logan <loganetherton@gmail.com>
2014-12-01 15:32:07 -0500

    Basic test for AcSocketService

---
+++ packages/custom/acsocket/public/tests/AcSocketService.js
@@ -0,0 +1,30 @@
+describe('AcSocketService', function () {
+    var acSocketService, rootScope;
+    beforeEach(function () {
+        module('mean');
+        module('mean.system');
+        module('mean.acsocket', function ($provide) {
+            $provide.factory('User', UserMock);
+        });
+    });
+    beforeEach(inject(function (AcSocketService, $rootScope) {
+        acSocketService = AcSocketService;
+        rootScope = $rootScope;
+    }));
+
+    it('should have getSocket, on, and emit functions', function () {
+        expect(acSocketService.getSocket).toBeTruthy();
+        expect(acSocketService.on).toBeTruthy();
+        expect(acSocketService.emit).toBeTruthy();
+    });
+
+    it('should call getSocket() to reference the correct socket on all emit() and on() calls', function () {
+        spyOn(acSocketService, 'getSocket').andCallThrough();
+        // Attach event listener
+        acSocketService.on('test', function () {});
+        expect(acSocketService.getSocket).toHaveBeenCalled();
+        // Emit
+        acSocketService.emit('test');
+        expect(acSocketService.getSocket).toHaveBeenCalled();
+    });
+});

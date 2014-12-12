a7f6b120705661c991e9dae24527e7f30f8676a8
logan <loganetherton@gmail.com>
2014-12-12 14:28:27 -0500

    Fixing layout issues with container items

--- packages/system/public/controllers/index.js
+++ packages/system/public/controllers/index.js
@@ -4,15 +4,17 @@
 var app = angular.module('mean.system');
 
 app.controller('IndexController', ['$scope', 'Global', '$location', function ($scope, Global, $location) {
+    var vm = this;
     $scope.global = Global;
 
-    $scope.isSpecificPage = function() {
+    vm.isSpecificPage = function() {
         var path;
         path = $location.path();
-        return _.contains(['/404', '/pages/500', '/pages/login', '/pages/signin', '/pages/signin1', '/pages/signin2', '/pages/signup', '/pages/signup1', '/pages/signup2', '/pages/lock-screen'], path);
+        return _.contains(['/404', '/pages/500', '/pages/login', '/pages/signin', '/pages/signin1', '/pages/signin2', '/pages/signup', '/pages/signup1', '/pages/signup2', '/pages/lock-screen', '/auth/login'], path);
     };
-    $scope.main = {
-        brand: 'Flatify',
+
+    vm.main = {
+        brand: 'dry',
         name: 'Lisa Doe'
     };
 }]);

1fe637580ea01f562582f427dc52921fc53a5fea
logan <loganetherton@gmail.com>
2015-02-02 01:04:42 -0500

    Updating loginUser for mocha to use promises

--- test/mochaHelpers/loginUser.js
+++ test/mochaHelpers/loginUser.js
@@ -1,3 +1,4 @@
+var q = require('q');
 /**
  * Log the user in
  * @param email
@@ -5,15 +6,22 @@
  * @param done
  */
 module.exports = function (server, email, password, done) {
+    var deferred = q.defer();
     server
     .post('/login')
     .send({ email: email, password: password })
     .expect(200)
     .end(function (err, res) {
         if (err) {
+            console.log('**************ERROR**********');
+            console.log(err);
             return done(err);
         }
         res.body.user._id.should.be.ok;
-        return done();
+        deferred.resolve();
+        if (typeof done === 'function') {
+            return done();
+        }
     });
+    return deferred.promise;
 };
